68652485ca1e172c3c3f248bae6c7ba20bce4e4b
Md Emon Hossain <96044760+S1Emon@users.noreply.github.com>
2022-05-29 14:54:18 +0600

    google login added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import auth from '../../Firebase/Firebase.init';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 
 const Login = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+    if (user) {
+        console.log(user);
+    }
     return (
-        <div>
-            <h1>This is login Section</h1>
+        <div className='flex justify-center items-center h-screen'>
+            <div class="card w-96 bg-base-100 shadow-xl">
+                <div class="card-body">
+                    <h2 class="text-center font-bold text-2xl">Login</h2>
+
+                    <div class="divider">OR</div>
+                    <button
+                        onClick={() => signInWithGoogle()}
+                        class="btn btn-outline btn-accent"
+                    >Continue with Google</button>
+                </div>
+            </div>
         </div>
     );
 };
