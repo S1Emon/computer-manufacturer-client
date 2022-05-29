ffcc7e0ab74631015c423e403fbc527bc604abcf
Md Emon Hossain <96044760+S1Emon@users.noreply.github.com>
2022-05-30 02:33:26 +0600

    single data load

--- src/Pages/Dashboard/MyOrders.js
+++ src/Pages/Dashboard/MyOrders.js
@@ -1,16 +1,35 @@
+import { signOut } from 'firebase/auth';
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
 import auth from '../../Firebase/Firebase.init';
 
 const MyOrders = () => {
     const [orders, setOrders] = useState([])
     const [user] = useAuthState(auth);
+    const navigate = useNavigate()
+    console.log(user);
 
     useEffect(() => {
         if (user) {
-            fetch(`http://localhost:5000/orders`)
-                .then(res => res.json())
-                .then(data => setOrders(data))
+            fetch(`http://localhost:5000/orders?email=${user.email}`, {
+                method: 'GET',
+                headers: {
+                    'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+                }
+            })
+                .then(res => {
+                    console.log('res', res);
+                    if (res.status === 401 || res.status === 403) {
+                        signOut(auth);
+                        localStorage.removeItem('accessToken');
+                        navigate('/');
+                    }
+                    return res.json()
+                })
+                .then(data => {
+                    setOrders(data);
+                });
         }
     }, [user])
     return (
