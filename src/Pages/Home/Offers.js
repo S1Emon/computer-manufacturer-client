ccd2a5867f35681d91a9615d6f558258e9d901c8
Md Emon Hossain <96044760+S1Emon@users.noreply.github.com>
2022-06-07 09:46:01 +0600

    second

---
+++ src/Pages/Home/Offers.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Offers = () => {
+    return (
+        <div>
+            <h1>This is Offer Sections!!</h1>
+        </div>
+    );
+};
+
+export default Offers;
